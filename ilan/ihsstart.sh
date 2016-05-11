1d825a9bb8f6edf1148abf462dd603f8813b5739
Kavitha Suresh Kumar <kavisuresh@in.ibm.com>
2016-05-11 13:42:20 +0530

    Script modified to gracefully stop

--- ilan/ihsstart.sh
+++ ilan/ihsstart.sh
@@ -7,10 +7,34 @@
 #                                                                                   #
 #####################################################################################
 
-# Starting IBM HTTPServer
-/opt/IBM/HTTPServer/bin/apachectl start
+startServer()
+{
+    echo "Starting IBM HTTP Server "
+    # Starting IBM HTTPServer
+    /opt/IBM/HTTPServer/bin/apachectl start
 
-echo "IBM HTTP Server started successfully"
+    if [ $? = 0 ]
+    then
+       echo "IBM HTTP Server started successfully"
+    else
+       echo "Failed to start IBM HTTP Server"
+    fi
+}
+
+stopServer()
+{
+    echo "Stopping IBM HTTP Server "
+    # Stopping IBM HTTPServer
+    /opt/IBM/HTTPServer/bin/apachectl stop
+    if [ $? = 0 ]
+    then
+       echo "IBM HTTP Server stopped successfully"
+    fi
+}
+
+startServer
+
+trap "stopServer" SIGTERM SIGKILL 
 
 while [ `ps -eaf | grep httpd | wc -l` > 4 ]
 do
